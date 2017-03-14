bd94090c11ec334c30d126fb5756307cf05db0f1
Mr.Viana <rafaelgov95@gmail.com>
2017-03-14 18:12:36 -0400

    Update

--- Trabalho - B/B/src/br/ufms/b/app/CriaArvoreB.java
+++ Trabalho - B/B/src/br/ufms/b/app/CriaArvoreB.java
@@ -1,4 +1,5 @@
 package br.ufms.b.app;
+
 import br.ufms.b.model.controller.ArvoreB;
 import br.ufms.b.model.Pag;
 import java.io.*;
@@ -7,30 +8,34 @@ public class CriaArvoreB {
 
     public static void main(String[] args) throws Exception {
         ArvoreB b = new ArvoreB(2);
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader leia = new BufferedReader(new InputStreamReader(System.in));
         boolean flagPrincipal = true;
         while (flagPrincipal) {
-            System.out.println("-------------Menu De Criação Arvore B---------\n"
-                    + "----------- 1 - Criar B        ----------\n"
-                    + "----------- 2 - Busca B        ----------\n"
-                    + "----------- 3 - Remover B        ----------\n"
-                    + "----------- 0 - Sair        ----------\n");
-            String opcao = in.readLine();
+            System.out.print("-------------Menu De Criação Arvore B---------\n"
+                    + "--------------    1 - Criar B      -----------\n"
+                    + "--------------    2 - Busca B     ------------\n"
+                    + "--------------    3 - Remover B   ------------\n"
+                    + "--------------    0 - Sair        ------------\n"
+                    + "Escolha uma das opcoes acima: ");
+
+            String opcao = leia.readLine();
+
             int chave;
             switch (opcao) {
                 case "1":
-                    System.out.println("Informe um elemento");
-                    chave = Integer.parseInt(in.readLine());
+                    System.out.println("Informe um elemento || 0 para sair !");
+                    chave = Integer.parseInt(leia.readLine());
                     while (chave > 0) {
                         Pag item = new Pag(chave);
                         b.inserir(item);
                         b.print();
-                        chave = Integer.parseInt(in.readLine());
+                        chave = Integer.parseInt(leia.readLine());
+                        System.out.println("Informe um elemento || 0 para sair !");
                     }
                     break;
                 case "2":
                     System.out.println("\nPesquisando chaves");
-                    chave = Integer.parseInt(in.readLine());
+                    chave = Integer.parseInt(leia.readLine());
                     while (chave > 0) {
                         Pag item = new Pag(chave);
                         item = (Pag) b.busca(item);
@@ -39,19 +44,21 @@ public class CriaArvoreB {
                         } else {
                             System.out.println("Item encontrado");
                         }
-                        chave = Integer.parseInt(in.readLine());
+                        chave = Integer.parseInt(leia.readLine());
                     }
                     break;
                 case "3":
                     System.out.println("\nRemovendo algumas chaves");
-                    chave = Integer.parseInt(in.readLine());
+                    chave = Integer.parseInt(leia.readLine());
                     while (chave > 0) {
                         Pag item = new Pag(chave);
                         b.remover(item);
                         b.print();
-                        chave = Integer.parseInt(in.readLine());
+                        chave = Integer.parseInt(leia.readLine());
                     }
                     break;
+                default:
+                    System.out.println("Opção Invalida!!!");
 
             }
         }
